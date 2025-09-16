b3a9f469aa71159e7794b76e79ab5fd3fc865e3d
Roman Volykh <rvolykh@gmail.com>
2025-09-16 13:29:50 +0300

    fix: [math] Resolve math runtime error

--- math/cmd/main.go
+++ math/cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -11,6 +10,7 @@ import (
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 	"github.com/rvolykh/helloworld-mcp/math/tools"
 	"github.com/rvolykh/helloworld-mcp/shared/middleware"
+	"github.com/rvolykh/helloworld-mcp/shared/wrapper"
 )
 
 var (
@@ -33,34 +33,27 @@ func main() {
 
 	url := fmt.Sprintf("%s:%d", *host, *port)
 
-	// Create a server with tools.
+	// Create a server
 	server := mcp.NewServer(&mcp.Implementation{Name: "math", Version: "v1.0.0"}, nil)
 
-	tools := map[string][]any{
-		"Max":   {tools.MaxDescription, tools.Max},
-		"Min":   {tools.MinDescription, tools.Min},
-		"Abs":   {tools.AbsDescription, tools.Abs},
-		"Sqrt":  {tools.SqrtDescription, tools.Sqrt},
-		"Pow":   {tools.PowDescription, tools.Pow},
-		"Sin":   {tools.SinDescription, tools.Sin},
-		"Cos":   {tools.CosDescription, tools.Cos},
-		"Tan":   {tools.TanDescription, tools.Tan},
-		"Log":   {tools.LogDescription, tools.Log},
-		"Log10": {tools.Log10Description, tools.Log10},
-		"Exp":   {tools.ExpDescription, tools.Exp},
-		"Round": {tools.RoundDescription, tools.Round},
-		"Trunc": {tools.TruncDescription, tools.Trunc},
-		"Mod":   {tools.ModDescription, tools.Mod},
-		"Hypot": {tools.HypotDescription, tools.Hypot},
-		"Floor": {tools.FloorDescription, tools.Floor},
-		"Ceil":  {tools.CeilDescription, tools.Ceil},
-	}
-	for name, tool := range tools {
-		mcp.AddTool(server, &mcp.Tool{
-			Name:        name,
-			Description: tool[0].(string),
-		}, WrapMathTool(tool[1].(func(context.Context, any) (any, error))))
-	}
+	// Add tools
+	mcp.AddTool(server, &mcp.Tool{Name: "Abs", Description: tools.AbsDescription}, wrapper.WrapMCPTool(tools.Abs))
+	mcp.AddTool(server, &mcp.Tool{Name: "Ceil", Description: tools.CeilDescription}, wrapper.WrapMCPTool(tools.Ceil))
+	mcp.AddTool(server, &mcp.Tool{Name: "Cos", Description: tools.CosDescription}, wrapper.WrapMCPTool(tools.Cos))
+	mcp.AddTool(server, &mcp.Tool{Name: "Exp", Description: tools.ExpDescription}, wrapper.WrapMCPTool(tools.Exp))
+	mcp.AddTool(server, &mcp.Tool{Name: "Floor", Description: tools.FloorDescription}, wrapper.WrapMCPTool(tools.Floor))
+	mcp.AddTool(server, &mcp.Tool{Name: "Hypot", Description: tools.HypotDescription}, wrapper.WrapMCPTool(tools.Hypot))
+	mcp.AddTool(server, &mcp.Tool{Name: "Log", Description: tools.LogDescription}, wrapper.WrapMCPTool(tools.Log))
+	mcp.AddTool(server, &mcp.Tool{Name: "Log10", Description: tools.Log10Description}, wrapper.WrapMCPTool(tools.Log10))
+	mcp.AddTool(server, &mcp.Tool{Name: "Max", Description: tools.MaxDescription}, wrapper.WrapMCPTool(tools.Max))
+	mcp.AddTool(server, &mcp.Tool{Name: "Min", Description: tools.MinDescription}, wrapper.WrapMCPTool(tools.Min))
+	mcp.AddTool(server, &mcp.Tool{Name: "Mod", Description: tools.ModDescription}, wrapper.WrapMCPTool(tools.Mod))
+	mcp.AddTool(server, &mcp.Tool{Name: "Pow", Description: tools.PowDescription}, wrapper.WrapMCPTool(tools.Pow))
+	mcp.AddTool(server, &mcp.Tool{Name: "Round", Description: tools.RoundDescription}, wrapper.WrapMCPTool(tools.Round))
+	mcp.AddTool(server, &mcp.Tool{Name: "Sin", Description: tools.SinDescription}, wrapper.WrapMCPTool(tools.Sin))
+	mcp.AddTool(server, &mcp.Tool{Name: "Sqrt", Description: tools.SqrtDescription}, wrapper.WrapMCPTool(tools.Sqrt))
+	mcp.AddTool(server, &mcp.Tool{Name: "Tan", Description: tools.TanDescription}, wrapper.WrapMCPTool(tools.Tan))
+	mcp.AddTool(server, &mcp.Tool{Name: "Trunc", Description: tools.TruncDescription}, wrapper.WrapMCPTool(tools.Trunc))
 
 	// Run the server over HTTP
 	handler := mcp.NewStreamableHTTPHandler(func(req *http.Request) *mcp.Server {
@@ -72,15 +65,5 @@ func main() {
 	// Start the HTTP server with logging handler.
 	if err := http.ListenAndServe(url, handlerWithLogging); err != nil {
 		log.Fatalf("Server failed: %v", err)
-	}
-}
-
-func WrapMathTool[I any, O any](
-	toolFunc func(context.Context, I) (O, error),
-) func(context.Context, *mcp.CallToolRequest, I) (*mcp.CallToolResult, O, error) {
-
-	return func(ctx context.Context, _ *mcp.CallToolRequest, i I) (*mcp.CallToolResult, O, error) {
-		o, err := toolFunc(ctx, i)
-		return nil, o, err
 	}
 }
--- math/go.mod
+++ math/go.mod
@@ -3,11 +3,11 @@ module github.com/rvolykh/helloworld-mcp/math
 go 1.25.1
 
 require (
-	github.com/modelcontextprotocol/go-sdk v0.4.0
-	github.com/rvolykh/helloworld-mcp/shared v0.1.0
+	github.com/modelcontextprotocol/go-sdk v0.5.0
+	github.com/rvolykh/helloworld-mcp/shared v1.0.0
 )
 
 require (
-	github.com/google/jsonschema-go v0.2.1-0.20250825175020-748c325cec76 // indirect
+	github.com/google/jsonschema-go v0.2.3 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 )
